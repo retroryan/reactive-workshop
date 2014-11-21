9e669fdf1aa35f921556877d8e69ce2b299d366c
Russ White <russ.white@lifeway.com>
2014-11-21 11:49:03 -0600

    fixed tests

--- test/IntegrationSpec.scala
+++ test/IntegrationSpec.scala
@@ -16,7 +16,7 @@ class IntegrationSpec extends WordSpec with MustMatchers {
     "work from within a browser" in new WithBrowser {
 
       browser.goTo("http://localhost:" + port)
-      browser.pageSource must include ("Hello Play Framework")
+      browser.pageSource must include ("TweetMap")
     }
   }
 }
